b9b54089151501137b937c32a376bf38bfbafda8
Usha-SN <ushasnchaitra@gmail.com>
2022-01-09 21:55:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-fhsnqn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-fhsnqn)
---
+++ index.js
@@ -0,0 +1,42 @@
+// 1st assignment
+// add all the count values using for loop and console the output. output should be 6.
+const res = [
+  {
+    count: 1,
+  },
+  {
+    count: 2,
+  },
+  {
+    count: 3,
+  },
+];
+for(const i=1; i < count; i++){
+
+  var count = count[i] + count[i];
+
+  alert(count);
+}​
+// 2nd assignment
+// add all the count values using for loop and console the output. output should be 8.
+
+const response = {
+  maran: {
+    count: 1,
+  },
+  gas: {
+    count: 2,
+  },
+  prelude: {
+    count: 5,
+  },
+  keys: ['maran', 'gas', 'prelude'],
+};
+for(const i=1; i < count; i++){
+
+  var count = count[i] + count[i];
+
+  alert(count);
+}
+// 3rd assignment
+// change the 'HI' Text color on clicking of button.
